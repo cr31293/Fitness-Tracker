e0510e8954d379196eb551a0c4f9d70631eeb6bf
Chris Reed <cr31293@gmail.com>
2020-10-26 14:34:54 -0400

    correcting db routing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://cr31293:Columbia1808@cluster0.ftj0e.mongodb.net/workouts?retryWrites=true&w=majority"
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://cr31293:Coltsfan1808@cluster0.ftj0e.mongodb.net/workouts?retryWrites=true&w=majority"
 , {useNewUrlParser: true});
 
 app.use(require("./routes/api.js"));
